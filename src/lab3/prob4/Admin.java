76ca26af2797296e2b82fbfc522633305159a1ad
Sudipto Roy <sroy@miu.edu>
2025-01-18 14:24:16 -0600

    lab 4

--- src/lab3/prob4/Admin.java
+++ src/lab3/prob4/Admin.java
@@ -1,6 +1,7 @@
 package lab3.prob4;
 
 public class Admin {
+    public  int hello;
     public static double computeTotalRent(Property[] properties) {
         double totalRent = 0;
         for (Property property : properties) {
