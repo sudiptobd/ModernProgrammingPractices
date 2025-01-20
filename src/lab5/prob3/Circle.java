d8c9b991dac2faf8c07fa179c60d31e91f9e057f
Sudipto Roy <sroy@miu.edu>
2025-01-20 09:14:27 -0600

    lab 5

--- src/lab5/prob3/Circle.java
+++ src/lab5/prob3/Circle.java
@@ -1,4 +1,17 @@
 package lab5.prob3;
 
-public class Circle {
+public final class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double computeArea() {
+        return Math.PI * radius * radius;
+    }
 }
