4d90182b62e70a45307a746c0f0abbe7aa915750
Sudipto Roy <sroy@miu.edu>
2025-01-23 13:25:22 -0600

    lab 7 problem 1

---
+++ src/lab7/prob2/Main.java
@@ -0,0 +1,10 @@
+package lab7.prob2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+    }
+}
