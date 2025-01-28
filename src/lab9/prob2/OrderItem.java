b88423ec3e9c2c0befe15d0ceb55f2e26811051c
Sudipto Roy <sroy@miu.edu>
2025-01-28 09:13:46 -0600

    lab9 prob2

---
+++ src/lab9/prob2/OrderItem.java
@@ -0,0 +1,18 @@
+package lab9.prob2;
+
+public class OrderItem {
+	private String productId;
+	private int quantityRequested;
+	private int unitPrice;
+	//package level constructor
+	OrderItem(String id, int quantity, int price) {
+		this.productId = id;
+		this.quantityRequested = quantity;
+		this.unitPrice = price;
+	}
+	@Override
+	public String toString() {
+		return "   " + productId + ": " + quantityRequested + ", " + unitPrice;
+	}
+	
+}
