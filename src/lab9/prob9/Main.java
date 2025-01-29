c2fece3b37cb958faeaa75cb4d450632b34bd614
Sudipto Roy <sroy@miu.edu>
2025-01-29 14:29:06 -0600

    lab 9 part 2 prob 9

---
+++ src/lab9/prob9/Main.java
@@ -0,0 +1,19 @@
+package lab9.prob9;
+
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+        printSquares(4); // Output: 1, 4, 9, 16
+    }
+
+    public static void printSquares(int num) {
+        IntStream.iterate(1, Main::squareGenerator) // Generate squares
+                .limit(num)
+                .forEach(System.out::println);
+    }
+
+    public static int squareGenerator(int n) {
+        return n + (2 * (int) Math.sqrt(n) + 1);
+    }
+}
